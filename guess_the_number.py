8aac1882adee0b8f0d7e75c81489412469f7523c
DAV1NC11 <56307893+DAV1NC11@users.noreply.github.com>
2020-03-10 17:45:44 +0300

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,26 @@
+import random
+actual_value = random.randrange(1, 20)
+actual_value = int(actual_value)
+guess = int(input('Guess a number between 1 and 20: '))
+game = False
+while True:
+    if (float(guess)>int(actual_value)):
+        print('The value you are looking for is lower than this!')
+        guess = int(input('> '))
+        game = True
+
+    elif guess not in range(1, 21, 1):
+        print('Please read the instructions very carefully')
+        guess = int(input('> '))
+        game = True
+
+    elif (float(guess)<float(actual_value)):
+        print('THe value you are looking for is higher than this!')
+        guess = int(input('> '))
+        game = True
+        
+
+    else:
+        print('You guessed the right number!')
+        game = False
+        quit()
